ac647ffd64dfe0bfdece1fc2ce08fc43d90e2364
Mourad EL CADI <mouradface1998@gmail.com>
2022-06-14 22:53:28 +0100

    array pointers

--- test-code/pointers.c
+++ test-code/pointers.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-void pointer_casting(){
+void pointer_casting()
+{
     unsigned  int x = 126;
     unsigned int *p = NULL;
     p = &x;
@@ -18,6 +19,70 @@ void pointer_casting(){
     printf("%c \n", *q); // ==> '~'
 }
 
+void array_pointers()
+{
+    int A[5] = {0, 1, 2, 3, 4};
+    int *p = NULL;
+    // p = &A[0];
+    p = A; // name of the array is a pointer itself! (can be used as a pointer)
+    // but we can't use it for arithmitics (A++, A = A+2 ...)
+    
+    // (p+i) <==> (A+i) <==> &A[i]
+    // *(p+i) <==> *(A+i) <==> A[i]
+
+    printf("%d \n", *(p + 1));
+    printf("%d \n", *(A + 2)); // same as A[2]
+
+    return 0;
+}
+
+void increment(int *p)
+{
+    (*p)++;
+}
+
+int array_sum(int A[]) // array_sum(int* A)
+{
+/*
+    // in fact the array is passed to the function by refrence (address)
+    // it's memory-expensive to copy an array everytime ... so only refrence is passed
+    // passing an array is same as (int* A) --A is only the address-- Try 
+    int x = 100;
+    int *p = NULL;
+    p = &x;
+    printf("%d \n", p[0]); // pointers and array-names are almost the same
+*/
+
+    int i, sum = 0;
+    int length = sizeof(A) / sizeof(int); // this will give 1 because A is only a pointer
+    // size of the array should be the second argument of the function it should have been 
+    // calculated in main array_sum(int *A, size)
+
+    for(i=0; i<length; ++i)
+    {
+        sum += A[i];
+    }
+
+    return sum;
+}
+
+int array_double(int A[], int length) // array_sum(int* A)
+{
+    int i;
+
+    for(i=0; i<length; ++i)
+    {
+        A[i] *= 2;
+    }
+}
+
+void string_array()
+{
+    char C[5] = {'M', 'o', 'd', '\0'}; // '\0' should be added explicitly
+    char B = "Mod"; // '\0' is added by default implicitly
+
+}
+
 void main(void)
 {
     /*
@@ -28,11 +93,21 @@ void main(void)
         the binary value to get the original value (int != float != unsigned double ...)
     */
 
+   /*
     char str[] = "Mourad";
     char c = 'o';
     char *p;
 
     p = strchr(str, c);
+    */
 
-    printf("hello \n");
+   int a = 10;
+   increment(&a);
+   // printf("a = %d \n", a);
+
+    int a[5] = {1, 2, 3, 4, 5};
+
+    // ************************************
+    int A[] = {1, 2, 3, 4, 5};
+    array_sum(A); // only address is passed in fact
 }
